e5dc906c136852d5dd1b8672d2b146536562c553
SnusnumrConceit <snusnumr1@gmail.com>
2021-04-22 00:14:36 +0400

    User: миграция полей, полнотекстового индекса и внешнего ключа к ролям

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -15,13 +16,25 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->uuid('uuid')->index();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('last_name', 40);
+            $table->string('first_name', 40);
+            $table->string('middle_name', 40);
             $table->string('password');
+            $table->string('appointment', 50);
+            $table->unsignedTinyInteger('role_id')->nullable();
+//            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('SET NULL');
         });
+
+        DB::statement('CREATE FULLTEXT INDEX fio_index ON users(last_name, first_name, middle_name)');
     }
 
     /**
@@ -31,6 +44,13 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        DB::statement('ALTER TABLE users DROP INDEX fio_index');
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
+            $table->dropIndex(['uuid']);
+        });
+
         Schema::dropIfExists('users');
     }
 }

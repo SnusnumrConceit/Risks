dc8b74f47b6ee4220a0131159e8b2b94e9cbeae9
SnusnumrConceit <snusnumr1@gmail.com>
2021-05-15 05:55:42 +0400

    Charts: базовый компонент графика Pie

---
+++ resources/js/components/charts/Base/Pie.vue
@@ -0,0 +1,20 @@
+<script>
+    import { Pie } from 'vue-chartjs';
+
+    export default {
+        extends: Pie,
+        name: "pie",
+        props: ['labels', 'datasets', 'options'],
+        mounted() {
+            console.log({labels: this.labels, datasets: this.datasets});
+            this.renderChart(
+                {
+                  labels: this.labels,
+                  datasets: this.datasets
+                },
+              this.options
+            );
+
+        }
+    }
+</script>

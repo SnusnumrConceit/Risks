fa5503e05404c3a6597b9b8ec98e4075cd155bca
SnusnumrConceit <snusnumr1@gmail.com>
2021-05-19 00:48:42 +0400

    Metrics: проверка наличия метрик в шаблоне

--- resources/views/metrics.blade.php
+++ resources/views/metrics.blade.php
@@ -15,77 +15,88 @@
                     <h2>{{ __('risks.risks') }}</h2>
                     <div class="form-group mb-4 pb-4 border-bottom">
                         <div class="row my-4">
+                            @if($risks_amount)
                             {{-- Суммарная метрика по рискам --}}
-                            <div class="col-md-6">
-                                <risks-amount-metric
-                                    :headers="['{{ __('risks.risks') }}']"
-                                    label="{{ __('risks.risks') }}"
-                                    :data="[{{ $risks_amount }}]"
-                                    :colors="['rgb(54, 162, 235)']"  синий
-                                />
-                            </div>
-                            {{-- Суммарная метрика по головным подразделениям --}}
-                            <div class="col-md-6">
-                                <risks-divisions-metric
-                                    :headers="['{{ implode("', '", array_keys($risks_divisions_metric)) }}']"
-                                    label="{{ __('divisions.divisions') }}"
-                                    :data="[{{ implode(',', $divisions = array_values($risks_divisions_metric)) }}]"
-                                    :colors="[
-                                @foreach($divisions as $division)
-                                        '{{ generate_color() }}',
-                                @endforeach
-                                        ]"
-                                />
-                            </div>
+                                <div class="col-md-6">
+                                    <risks-amount-metric
+                                        :headers="['{{ __('risks.risks') }}']"
+                                        label="{{ __('risks.risks') }}"
+                                        :data="[{{ $risks_amount }}]"
+                                        :colors="['rgb(54, 162, 235)']"  синий
+                                    />
+                                </div>
+                            @endif
+                            @if($risks_divisions_metric)
+                                {{-- Суммарная метрика по головным подразделениям --}}
+                                <div class="col-md-6">
+                                    <risks-divisions-metric
+                                        :headers="['{{ implode("', '", array_keys($risks_divisions_metric)) }}']"
+                                        label="{{ __('divisions.divisions') }}"
+                                        :data="[{{ implode(',', $divisions = array_values($risks_divisions_metric)) }}]"
+                                        :colors="[
+                                    @foreach($divisions as $division)
+                                            '{{ generate_color() }}',
+                                    @endforeach
+                                            ]"
+                                    />
+                                </div>
+                            @endif
                         </div>
 
                         <div class="row my-4">
-                            {{-- Суммарная метрика по видам рисков --}}
-                            <div class="col-md-6">
-                                <risks-types-metric
-                                    :headers="['{{ implode("', '", array_keys($risks_types_metric)) }}']"
-                                    label="{{ __('types.types') }}"
-                                    :data="[{{ implode(',', $types = array_values($risks_types_metric)) }}]"
-                                    :colors="[
-                                @foreach($types as $type)
-                                        '{{ generate_color() }}',
-                                @endforeach
-                                        ]"
-                                />
-                            </div>
-                            {{-- Суммарная метрика по факторам рисков --}}
-                            <div class="col-md-6">
-                                <risks-factors-metric
-                                    :headers="['{{ implode("', '", array_keys($risks_factors_metric)) }}']"
-                                    label="{{ __('factors.factors') }}"
-                                    :data="[{{ implode(',', $factors = array_values($risks_factors_metric)) }}]"
-                                    :colors="[
-                                @foreach($factors as $factor)
-                                        '{{ generate_color() }}',
-                                @endforeach
-                                        ]"
-                                />
-                            </div>
+                            @if($risks_types_metric)
+                                {{-- Суммарная метрика по видам рисков --}}
+                                <div class="col-md-6">
+                                    <risks-types-metric
+                                        :headers="['{{ implode("', '", array_keys($risks_types_metric)) }}']"
+                                        label="{{ __('types.types') }}"
+                                        :data="[{{ implode(',', $types = array_values($risks_types_metric)) }}]"
+                                        :colors="[
+                                    @foreach($types as $type)
+                                            '{{ generate_color() }}',
+                                    @endforeach
+                                            ]"
+                                    />
+                                </div>
+                            @endif
+
+                            @if($risks_factors_metric)
+                                {{-- Суммарная метрика по факторам рисков --}}
+                                <div class="col-md-6">
+                                    <risks-factors-metric
+                                        :headers="['{{ implode("', '", array_keys($risks_factors_metric)) }}']"
+                                        label="{{ __('factors.factors') }}"
+                                        :data="[{{ implode(',', $factors = array_values($risks_factors_metric)) }}]"
+                                        :colors="[
+                                    @foreach($factors as $factor)
+                                            '{{ generate_color() }}',
+                                    @endforeach
+                                            ]"
+                                    />
+                                </div>
+                            @endif
                         </div>
 
                         <div class="row my-4">
-                            {{-- Суммарная метрика по статусам рисков --}}
-                            <div class="col-md-6">
-                                <risks-statuses-metric
-                                    :headers="['{{ implode("', '", $statuses = array_keys($risks_statuses_metric)) }}']"
-                                    label="{{ __('factors.factors') }}"
-                                    :data="[{{ implode(',', array_values($risks_statuses_metric)) }}]"
-                                    :colors="[
-                                @foreach($statuses as $status)
-                                        '{{ $statusesColors[$status] }}',
-                                @endforeach
-                                        ]"
-                                />
-                            </div>
+                            @if($risks_statuses_metric)
+                                {{-- Суммарная метрика по статусам рисков --}}
+                                <div class="col-md-6">
+                                    <risks-statuses-metric
+                                        :headers="['{{ implode("', '", $statuses = array_keys($risks_statuses_metric)) }}']"
+                                        label="{{ __('factors.factors') }}"
+                                        :data="[{{ implode(',', array_values($risks_statuses_metric)) }}]"
+                                        :colors="[
+                                    @foreach($statuses as $status)
+                                            '{{ $statusesColors[$status] }}',
+                                    @endforeach
+                                            ]"
+                                    />
+                                </div>
+                            @endif
                         </div>
                     </div>
 
-                    @if(auth()->user()->hasPermission('users_view'))
+                    @if(auth()->user()->hasPermission('users_view') && boolval($users_amount))
                         <div class="form-group">
                             <h2>{{ __('users.users') }}</h2>
                             <div class="row my-4">

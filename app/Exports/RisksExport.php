d0bf7f76622f90bfd92742a386931bd85524ec61
SnusnumrConceit <snusnumr1@gmail.com>
2021-06-04 01:26:23 +0400

    Reports: добавлена выборка по итогам. Добавление результирующей итоговой строки в хуке AfterSheet. Применение к ней стилей выравнивания по правому краю и выделения жирным. Установлен числовой формат для Рассчётных колонок. Добавлены методы надстройки для получения строк и столбцов. Формирование итоговых строк для Подразделений

--- app/Exports/RisksExport.php
+++ app/Exports/RisksExport.php
@@ -4,18 +4,23 @@ namespace App\Exports;
 
 use App\Risk;
 use Maatwebsite\Excel\Excel;
+use Illuminate\Support\Facades\DB;
 use App\Services\RiskExportDataService;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Illuminate\Contracts\Support\Responsable;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class RisksExport implements FromCollection, Responsable, WithHeadings, WithMapping, ShouldAutoSize, WithStyles
+class RisksExport implements FromCollection, Responsable, WithHeadings, WithMapping, ShouldAutoSize, WithStyles, WithEvents
 {
     use Exportable;
 
@@ -23,6 +28,7 @@ class RisksExport implements FromCollection, Responsable, WithHeadings, WithMapp
             $writerType = Excel::XLSX,
             $filters    = [],
             $risksByDivisions = null,
+            $totals = null,
             $divisionRows = [],
             $riskExportDataService;
 
@@ -40,6 +46,44 @@ class RisksExport implements FromCollection, Responsable, WithHeadings, WithMapp
 
         $this->riskExportDataService = app(RiskExportDataService::class);
         $this->fileName = $this->getPreparedFileName();
+    }
+
+    /**
+     * Регистрация событий
+     *
+     * @return array
+     */
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                /** добавление последней итоговой строки */
+                $event->sheet->appendRows(array(
+                    array_merge(
+                        array('Итого'),
+                        array_fill_keys(array_values($this->filters->cols), null),
+                        $this->totals['']->first()->only('summa', 'damage')
+                    )
+                ), $event);
+
+                /** @var  $totalColumnsLine - последняя строка "Итого" для мёржа колонок */
+                $totalColumnsLine = $this->getDivisionLine($event->sheet->getHighestRow(), $this->getDivisionEndsColumn($event->sheet));
+
+                $event->sheet->setMergeCells(array_merge($event->sheet->getMergeCells(), [$totalColumnsLine => $totalColumnsLine]));
+
+                /** применение стилей к последней итоговой строке */
+                $event->sheet->getStyle($this->getDivisionLine($event->sheet->getHighestRow(), $event->sheet->getHighestColumn()))->applyFromArray([
+                    'alignment' => ['horizontal' => Alignment::HORIZONTAL_RIGHT],
+                    'font' => ['bold' => true],
+                ]);
+
+                $event->sheet->getStyle(
+                    $this->getRowByRange($this->getColumnByEndIndex($event->sheet, 1), 2, $event->sheet->getHighestColumn(), $event->sheet->getHighestRow())
+                )->getNumberFormat()->applyFromArray([
+                    'formatCode' => NumberFormat::FORMAT_NUMBER_00
+                ]);
+            }
+        ];
     }
 
     /**
@@ -87,16 +131,16 @@ class RisksExport implements FromCollection, Responsable, WithHeadings, WithMapp
      */
     public function styles(Worksheet $sheet)
     {
-        $highestColumn = $sheet->getHighestColumn();
+        $divisionColumn = $this->getDivisionEndsColumn($sheet);
 
         foreach ($sheet->getRowIterator() as $row) {
             if (in_array($index = $row->getRowIndex(), $this->divisionRows)) {
-                $sheet->mergeCells($this->getDivisionLine($index, $highestColumn));
+                $sheet->mergeCells($this->getDivisionLine($index, $divisionColumn));
             }
         }
 
         $styles = array_fill_keys($this->divisionRows, [
-            'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER],
+            'alignment' => ['horizontal' => Alignment::HORIZONTAL_RIGHT],
             'font' => ['bold' => true],
         ]);
         $styles['1'] = ['font' => ['bold' => true]];
@@ -122,6 +166,15 @@ class RisksExport implements FromCollection, Responsable, WithHeadings, WithMapp
             ->all()
         );
 
+        $this->totals = Risk::with('division:id,name')
+            ->select(
+                'division_id',
+                DB::raw('SUM(summa) as summa'),
+                DB::raw('SUM(damage) as damage')
+            )->groupByRaw('division_id WITH ROLLUP')
+            ->get()
+            ->groupBy('division.name');
+
         return $this->risksByDivisions;
     }
 
@@ -143,6 +196,7 @@ class RisksExport implements FromCollection, Responsable, WithHeadings, WithMapp
 
             $records->prepend($division);
             $position += count($records);
+            $records->push($this->totals[$division]->first());
         }
 
         return $rows;
@@ -156,10 +210,42 @@ class RisksExport implements FromCollection, Responsable, WithHeadings, WithMapp
      */
     public function map($risksWithHeading): array
     {
+        $heading = $risksWithHeading->pull(0);
+        $totals = $risksWithHeading->pop()->only('summa', 'damage');
+
         return array_merge(
-            [[$risksWithHeading->pull(0)]],
-            $this->riskExportDataService->getRisksMappedData($risksWithHeading, $this->filters->cols)
+            [array_merge([$heading], array_fill_keys(array_values($this->filters->cols), null), $totals)],
+            $this->riskExportDataService->getRisksMappedData($risksWithHeading, $this->filters->cols),
         );
+    }
+
+    /**
+     * Получить колонку по индексу с конца таблицы
+     *
+     * @param $sheet
+     * @param $index
+     * @return string
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    protected function getColumnByEndIndex($sheet, $index)
+    {
+        return Coordinate::stringFromColumnIndex(
+            Coordinate::columnIndexFromString($sheet->getHighestColumn()) - $index
+        );
+    }
+
+    /**
+     * Получить координаты строк(-и)
+     *
+     * @param string $start
+     * @param int $startPos
+     * @param string $end
+     * @param int $endPos
+     * @return string
+     */
+    protected function getRowByRange(string $start, int $startPos, string $end, int $endPos)
+    {
+        return "{$start}{$startPos}:{$end}{$endPos}";
     }
 
     /**
@@ -171,6 +257,18 @@ class RisksExport implements FromCollection, Responsable, WithHeadings, WithMapp
      */
     protected function getDivisionLine(int $index, string $column)
     {
-        return "A{$index}:{$column}{$index}";
+        return $this->getRowByRange('A', $index, $column, $index);
+    }
+
+    /**
+     * Получить последнюю колонку с Подразделением
+     *
+     * @param $sheet
+     * @return string
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    protected function getDivisionEndsColumn($sheet)
+    {
+        return $this->getColumnByEndIndex($sheet, 2);
     }
 }
